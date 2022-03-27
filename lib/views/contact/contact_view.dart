50d69dda38d9d31d4bf448ab464437e786e307c2
alex90271 <alex.alder1@gmail.com>
2022-03-27 13:27:02 -0600

    additional foundation work

--- lib/views/contact/contact_view.dart
+++ lib/views/contact/contact_view.dart
@@ -1,7 +1,11 @@
 import 'package:alderautomationsdotcom/views/widgets/centerted_view/centerted_view.dart';
 import 'package:alderautomationsdotcom/views/widgets/return_button/return_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import '../widgets/NavigationBar/navigation_bar.dart';
+
+const squareapp =
+    " <!-- Start Square Appointments Embed Code --><script src='https://squareup.com/appointments/buyer/widget/be8krbfi80p3h4/GEHYJQKV3K0K5.js'></script><!-- End Square Appointments Embed Code --> ";
 
 class ContactView extends StatelessWidget {
   const ContactView({Key? key}) : super(key: key);
@@ -11,7 +15,12 @@ class ContactView extends StatelessWidget {
     return Scaffold(
       body: CenteredView(
         child: Column(
-          children: const [NaviBar(), Text('contact us'), ReturnButton()],
+          children: [
+            const NaviBar(),
+            const Text('contact us'),
+            const ReturnButton(),
+            Html(data: squareapp)
+          ],
         ),
       ),
     );
