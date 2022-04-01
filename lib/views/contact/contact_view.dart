7347774fac1bc81004195210b20670322c0ed0bf
alex90271 <alex.alder1@gmail.com>
2022-03-31 18:44:56 -0600

    implemented stateful contact form

--- lib/views/contact/contact_view.dart
+++ lib/views/contact/contact_view.dart
@@ -1,3 +1,4 @@
+import 'package:alderautomationsdotcom/globals.dart';
 import 'package:alderautomationsdotcom/views/widgets/calendar/calendar.dart';
 import 'package:alderautomationsdotcom/views/widgets/centerted_view/centerted_view.dart';
 import 'package:alderautomationsdotcom/views/widgets/return_button/return_button.dart';
@@ -12,17 +13,160 @@ class ContactView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CenteredView(
-        child: Column(
-          children: [
-            const NaviBar(),
-            const Text('contact us'),
-            CalendarWidget(),
-            const ReturnButton(),
-          ],
-        ),
+    return SizedBox(
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Contact Us'),
+          ),
+          Column(
+            children: [
+              Row(
+                children: const [
+                  TextContactForm(
+                    label: 'First Name',
+                    initial: '',
+                    helper: '',
+                  ),
+                  TextContactForm(
+                    label: 'Last Name',
+                    initial: '',
+                    helper: '',
+                  ),
+                  TextContactForm(
+                    label: 'Buisness Name',
+                    initial: '',
+                    helper: '',
+                  )
+                ],
+              ),
+              Row(
+                children: const [
+                  TextContactForm(
+                    label: 'Phone Number',
+                    initial: '',
+                    helper: '',
+                    length: 10,
+                  ),
+                  TextContactForm(
+                    label: 'Email',
+                    initial: '',
+                    helper: '',
+                  ),
+                ],
+              ),
+              const DateFeild(
+                label: 'Date',
+                initial: '',
+                helper: '',
+              )
+            ],
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class DateFeild extends StatefulWidget {
+  final label;
+  final initial;
+  final helper;
+  final length;
+
+  const DateFeild({
+    Key? key,
+    this.label,
+    this.initial,
+    this.helper,
+    this.length = 50,
+  }) : super(key: key);
+
+  @override
+  State<DateFeild> createState() => _DateFeildState();
+}
+
+class _DateFeildState extends State<DateFeild> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            width: 350,
+            child: TextFormField(
+              initialValue: widget.initial,
+              maxLength: widget.length,
+              decoration: InputDecoration(
+                labelText: widget.label,
+                labelStyle: const TextStyle(
+                  color: brandBlack,
+                ),
+                helperText: widget.helper,
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: brandBlue),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const CalendarWidget()
+      ],
+    );
+  }
+}
+
+class TextContactForm extends StatefulWidget {
+  final initial;
+  final label;
+  final length;
+  final helper;
+  const TextContactForm(
+      {Key? key,
+      required this.initial,
+      required this.label,
+      required this.helper,
+      this.length = 30})
+      : super(key: key);
+
+  @override
+  State<TextContactForm> createState() => _TextContactFormState();
+}
+
+class _TextContactFormState extends State<TextContactForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            width: 350,
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              initialValue: widget.initial,
+              maxLength: widget.length,
+              decoration: InputDecoration(
+                labelText: widget.label,
+                labelStyle: const TextStyle(
+                  color: brandBlack,
+                ),
+                helperText: widget.helper,
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: brandBlue),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
