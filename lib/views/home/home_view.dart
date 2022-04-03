320169b489b322eed1981f67c995e9407ea43bee
alex90271 <alex.alder1@gmail.com>
2022-04-03 15:51:18 -0600

    added stateful scrolling widget for anchor points

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,38 +1,14 @@
-import 'package:alderautomationsdotcom/widgets/navigation_bar/navigation_bar.dart';
-import 'package:responsive_builder/responsive_builder.dart';
 import 'package:flutter/material.dart';
-import 'home_content_mobile.dart';
-import 'home_content_desktop.dart';
+import 'home_desktop.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
-  final calltoAction = ('Book a consultation');
-  final courseDetails =
-      ('Optimize workflows, organize data, \nor redesign your online presence \nContact now for a brief consultation');
-
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            const NaviBar(),
-            SizedBox(height: size.height * 0.25),
-            ScreenTypeLayout(
-              mobile: HomeContentMobile(
-                callToAction: calltoAction,
-                courseDetails: courseDetails,
-              ),
-              desktop: HomeContentDesktop(
-                callToAction: calltoAction,
-                courseDetails: courseDetails,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+        body: Column(children: <Widget>[
+      HomeContentDesktop(),
+    ]));
   }
 }
