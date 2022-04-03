ece28b88c7a75979c41efc52e9bdf8e2af9896a8
alex90271 <alex.alder1@gmail.com>
2022-04-03 14:19:39 -0600

    about section

---
+++ lib/views/home/content/about/about.dart
@@ -0,0 +1,44 @@
+import 'package:alderautomationsdotcom/globals.dart';
+import 'package:alderautomationsdotcom/widgets/centerted_view/centerted_view.dart';
+import 'package:flutter/material.dart';
+
+class AboutView extends StatelessWidget {
+  const AboutView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height * 0.85,
+      color: brandBlack,
+      child: CenteredView(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+              child: Text(
+                'About',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: brandWhite, fontSize: 24),
+              ),
+            ),
+            Row(
+              children: [
+                Image.asset('AA_main.png'),
+                const SizedBox(
+                  width: 50,
+                ),
+                const Expanded(
+                  child: Text(
+                    'Hello\nmy name is Alex Alder \nI am a full stack developer I specialize in Flutter, Python, Java, and Wordpress, with familiarity with JavaScript, C++, PHP, and am constantly excited to learn more ',
+                    style: TextStyle(color: brandWhite, fontSize: 18),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
