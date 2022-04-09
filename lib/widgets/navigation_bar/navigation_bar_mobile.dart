36755638997c7aff4bd8ae244e305df89216ef4a
alex90271 <alex.alder1@gmail.com>
2022-04-09 02:22:21 -0600

    work in progress indicator for mobile nav bar

--- lib/widgets/navigation_bar/navigation_bar_mobile.dart
+++ lib/widgets/navigation_bar/navigation_bar_mobile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../globals.dart';
 import 'navbar_logo.dart';
 
 class NavigationBarMobile extends StatelessWidget {
@@ -13,7 +14,14 @@ class NavigationBarMobile extends StatelessWidget {
         children: <Widget>[
           IconButton(
             icon: const Icon(Icons.menu),
-            onPressed: () {},
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  backgroundColor: brandWhite,
+                  content: Text(
+                    'The mobile nav bar is a work in progress, for now please just scroll',
+                    style: TextStyle(color: brandBlack),
+                  )));
+            },
           ),
           const NavBarLogo()
         ],
