74e1b355d4b6e7b950be494fb1ac40c24df307e6
alex90271 <alex.alder1@gmail.com>
2022-04-09 02:23:34 -0600

    about page responsiveness

--- lib/views/home/content/about/about.dart
+++ lib/views/home/content/about/about.dart
@@ -1,6 +1,7 @@
 import 'package:alderautomationsdotcom/globals.dart';
 import 'package:alderautomationsdotcom/widgets/centerted_view/centerted_view.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class AboutView extends StatelessWidget {
   const AboutView({Key? key}) : super(key: key);
@@ -14,35 +15,65 @@ class AboutView extends StatelessWidget {
       child: CenteredView(
         child: Align(
           alignment: Alignment.center,
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                child: Text(
-                  'About',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: brandWhite, fontSize: 24),
-                ),
-              ),
-              Row(
-                children: [
-                  ClipOval(
-                      child: Image.asset(
-                    'headshot.jpeg',
-                    height: size.height / 2,
-                  )),
-                  const SizedBox(
-                    width: 50,
+          child: ScreenTypeLayout(
+            desktop: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                  child: Text(
+                    'About',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: brandWhite, fontSize: 24),
                   ),
-                  const Expanded(
-                    child: Text(
+                ),
+                Row(
+                  children: [
+                    ClipOval(
+                        child: Image.asset(
+                      'headshot.jpeg',
+                      height: size.height / 2,
+                    )),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    const Expanded(
+                      child: Text(
+                        'Hello\nmy name is Alex Alder \nI am a full stack developer I specialize in Flutter, Python, Java, and Wordpress, with familiarity with JavaScript, C++, PHP, and am constantly excited to learn more ',
+                        style: TextStyle(color: brandWhite, fontSize: 18),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            mobile: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                  child: Text(
+                    'About',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: brandWhite, fontSize: 24),
+                  ),
+                ),
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    ClipOval(
+                        child: Image.asset(
+                      'headshot.jpeg',
+                      height: 275,
+                    )),
+                    const Text(
                       'Hello\nmy name is Alex Alder \nI am a full stack developer I specialize in Flutter, Python, Java, and Wordpress, with familiarity with JavaScript, C++, PHP, and am constantly excited to learn more ',
                       style: TextStyle(color: brandWhite, fontSize: 18),
                     ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
