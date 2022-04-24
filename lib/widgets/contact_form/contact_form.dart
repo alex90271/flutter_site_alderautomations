8c263df017bdb8b234a038cbb7cb2cf30c8f753a
alex90271 <alex.alder1@gmail.com>
2022-04-24 08:39:46 -0600

    add scroll view to contact form

--- lib/widgets/contact_form/contact_form.dart
+++ lib/widgets/contact_form/contact_form.dart
@@ -43,171 +43,174 @@ class _TextContactFormState extends State<TextContactForm> {
       projType = '';
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            validator: (value) {
-              if (value!.isNotEmpty) {
-                return null;
-              } else {
-                return value;
-              }
-            },
-            onSaved: (value) {
-              fName = value!;
-            },
-            maxLength: length,
-            decoration: decoration('First Name'),
-            style: const TextStyle(
-              color: brandWhite,
-            ),
-          ),
-          TextFormField(
-            validator: (value) {
-              if (value!.isNotEmpty) {
-                return null;
-              } else {
-                return value;
-              }
-            },
-            onSaved: (value) {
-              lName = value!;
-            },
-            maxLength: length,
-            decoration: decoration('Last Name'),
-            style: const TextStyle(
-              color: brandWhite,
-            ),
-          ),
-          TextFormField(
-            validator: (value) {
-              if (value!.isNotEmpty) {
-                return null;
-              } else {
-                return value;
-              }
-            },
-            onSaved: (value) {
-              buisnessName = value!;
-            },
-            maxLength: length,
-            decoration: decoration('Buisness Name'),
-            style: const TextStyle(
-              color: brandWhite,
-            ),
-          ),
-          TextFormField(
-            validator: (value) {
-              if (value!.isNotEmpty) {
-                if (isNumeric(value)) {
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              validator: (value) {
+                if (value!.isNotEmpty) {
                   return null;
+                } else {
+                  return value;
                 }
-              } else {
-                return value;
-              }
-            },
-            onSaved: (value) {
-              phone = value!;
-            },
-            maxLength: length,
-            decoration: decoration('Phone'),
-            style: const TextStyle(
-              color: brandWhite,
+              },
+              onSaved: (value) {
+                fName = value!;
+              },
+              maxLength: length,
+              decoration: decoration('First Name'),
+              style: const TextStyle(
+                color: brandWhite,
+              ),
             ),
-          ),
-          TextFormField(
-            validator: (value) {
-              if (value!.isNotEmpty) {
-                if (isEmail(value)) {
+            TextFormField(
+              validator: (value) {
+                if (value!.isNotEmpty) {
                   return null;
+                } else {
+                  return value;
                 }
-              } else {
-                return value;
-              }
-            },
-            onSaved: (value) {
-              email = value!;
-            },
-            maxLength: length,
-            decoration: decoration('Email'),
-            style: const TextStyle(
-              color: brandWhite,
+              },
+              onSaved: (value) {
+                lName = value!;
+              },
+              maxLength: length,
+              decoration: decoration('Last Name'),
+              style: const TextStyle(
+                color: brandWhite,
+              ),
             ),
-          ),
-          TextFormField(
-            validator: (value) {
-              if (value!.isNotEmpty) {
-                return null;
-              } else {
-                return value;
-              }
-            },
-            onSaved: (value) {
-              projType = value!;
-            },
-            maxLength: length,
-            decoration: decoration('Project Type'),
-            style: const TextStyle(
-              color: brandWhite,
+            TextFormField(
+              validator: (value) {
+                if (value!.isNotEmpty) {
+                  return null;
+                } else {
+                  return value;
+                }
+              },
+              onSaved: (value) {
+                buisnessName = value!;
+              },
+              maxLength: length,
+              decoration: decoration('Buisness Name'),
+              style: const TextStyle(
+                color: brandWhite,
+              ),
             ),
-          ),
-          ElevatedButton(
-            //submit button
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(brandBlack)),
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                log('valid inputs');
-                _formKey.currentState!.save();
-                FirebaseFirestore.instance
-                    .collection("contact_form")
-                    .add({
-                      "firstname": fName,
-                      "lastname": lName,
-                      "buisnessname": buisnessName,
-                      "email": email,
-                      "phone": phone,
-                      "projectype": projType,
-                      "timestamp": DateTime.now(),
-                    })
-                    .then((value) => showDialog(
-                        context: context,
-                        builder: (BuildContext cxt) {
-                          return const ShowValidAlert(
-                            validHeader: "Success",
-                            validBody: "Your request has been saved",
-                          );
-                        }))
-                    .catchError((error) => showDialog(
-                        context: context,
-                        builder: (BuildContext cxt) {
-                          return const ShowValidAlert(
-                            validHeader: "Error",
-                            validBody:
-                                "There was an error with your request\nplease try again later\nor email us at alex@alderautomations.com",
-                          );
-                        }));
-              } else {
-                log('invalid inputs');
-                showDialog(
-                    context: context,
-                    builder: (BuildContext cxt) {
-                      return const ShowInvalidAlert(
-                        invalidHeader: "Error",
-                        invalidBody:
-                            "There was an error with your request\nplease try again later\nor email us at alex@alderautomations.com",
-                      );
-                    });
-              }
-            },
-            child: const Text(
-              'Submit',
-              style: TextStyle(color: brandWhite),
+            TextFormField(
+              validator: (value) {
+                if (value!.isNotEmpty) {
+                  if (isNumeric(value)) {
+                    return null;
+                  }
+                } else {
+                  return value;
+                }
+              },
+              onSaved: (value) {
+                phone = value!;
+              },
+              maxLength: length,
+              decoration: decoration('Phone'),
+              style: const TextStyle(
+                color: brandWhite,
+              ),
             ),
-          ),
-        ],
+            TextFormField(
+              validator: (value) {
+                if (value!.isNotEmpty) {
+                  if (isEmail(value)) {
+                    return null;
+                  }
+                } else {
+                  return value;
+                }
+              },
+              onSaved: (value) {
+                email = value!;
+              },
+              maxLength: length,
+              decoration: decoration('Email'),
+              style: const TextStyle(
+                color: brandWhite,
+              ),
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value!.isNotEmpty) {
+                  return null;
+                } else {
+                  return value;
+                }
+              },
+              onSaved: (value) {
+                projType = value!;
+              },
+              maxLength: length,
+              decoration: decoration('Project Type'),
+              style: const TextStyle(
+                color: brandWhite,
+              ),
+            ),
+            ElevatedButton(
+              //submit button
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(brandBlack)),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  log('valid inputs');
+                  _formKey.currentState!.save();
+                  FirebaseFirestore.instance
+                      .collection("contact_form")
+                      .add({
+                        "firstname": fName,
+                        "lastname": lName,
+                        "buisnessname": buisnessName,
+                        "email": email,
+                        "phone": phone,
+                        "projectype": projType,
+                        "timestamp": DateTime.now(),
+                      })
+                      .then((value) => showDialog(
+                          context: context,
+                          builder: (BuildContext cxt) {
+                            return const ShowValidAlert(
+                              validHeader: "Success",
+                              validBody: "Your request has been saved",
+                            );
+                          }))
+                      .catchError((error) => showDialog(
+                          context: context,
+                          builder: (BuildContext cxt) {
+                            return const ShowValidAlert(
+                              validHeader: "Error",
+                              validBody:
+                                  "There was an error with your request\nplease try again later\nor email us at alex@alderautomations.com",
+                            );
+                          }));
+                } else {
+                  log('invalid inputs');
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext cxt) {
+                        return const ShowInvalidAlert(
+                          invalidHeader: "Error",
+                          invalidBody:
+                              "There was an error with your request\nplease try again later\nor email us at alex@alderautomations.com",
+                        );
+                      });
+                }
+              },
+              child: const Text(
+                'Submit',
+                style: TextStyle(color: brandWhite),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
