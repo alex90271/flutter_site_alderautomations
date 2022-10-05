d519ed31e8a75fc09f9fbf472467677e357c593f
alex90271 <alex.alder1@gmail.com>
2022-10-04 23:53:32 -0400

    add text input commenting

--- lib/widgets/text_to_speech/text_to_speech.dart
+++ lib/widgets/text_to_speech/text_to_speech.dart
@@ -162,3 +162,18 @@ class _TextInputState extends State<TextInput> {
     );
   }
 }
+
+
+/*
+test input
+Here are <say-as interpret-as="characters">SSML</say-as> samples.
+I can pause <break time="3s"/>.
+I can play a sound
+<audio src="https://rpg.hamsterrepublic.com/wiki-images/d/db/Crush8-Bit.ogg">didn't get your MP3 audio file</audio>.
+I can speak in cardinals. Your number is <say-as interpret-as="cardinal">10</say-as>.
+Or I can speak in ordinals. You are <say-as interpret-as="ordinal">10</say-as> in line.
+Or I can even speak in digits. The digits for ten are <say-as interpret-as="characters">10</say-as>.
+I can also substitute phrases, like the <sub alias="World Wide Web Consortium">W3C</sub>.
+Finally, I can speak a paragraph with two sentences.
+<p><s>This is sentence one.</s><s>This is sentence two.</s></p>
+*/
