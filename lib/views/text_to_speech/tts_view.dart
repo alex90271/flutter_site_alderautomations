cf2308e8b0b5610e8b267bf4663ab18e091c1377
alex90271 <alex.alder1@gmail.com>
2022-10-05 23:13:02 -0400

    added button controls to inject SSML

--- lib/views/text_to_speech/tts_view.dart
+++ lib/views/text_to_speech/tts_view.dart
@@ -14,25 +14,31 @@ class TextToSpeechView extends StatelessWidget {
           child: Column(
             children: [
               const TextToSpeech(),
-              const Text(
-                  'To add a speaking break, write or copy / paste this line:'),
               const Padding(
                 padding: EdgeInsets.all(8.0),
-                child: SelectableText(
-                  "<break time=\"2s\"/>",
+                child: Text('To add a verbal pause, click one of the above',
+                    style: TextStyle(fontFamily: 'roboto')),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Replace the 1 or 2 within the <break time=\"2s\"/> with how many seconds to customize pause",
                   style: TextStyle(fontFamily: 'roboto'),
                 ),
               ),
-              const Text(
-                  "Replace the 2 with how many seconds the break needs to be"),
               const Padding(
                 padding: EdgeInsets.all(8.0),
-                child: Text("For more information on SSML visit:"),
+                child: Text(
+                  "For further customization, use SSML. For more information on SSML visit:",
+                  style: TextStyle(fontFamily: 'roboto'),
+                ),
               ),
               InkWell(
                 child: const Text(
                   'https://cloud.google.com/text-to-speech/docs/ssml',
-                  style: TextStyle(fontFamily: 'roboto'),
+                  style: TextStyle(
+                      fontFamily: 'roboto',
+                      decoration: TextDecoration.underline),
                 ),
                 onTap: () =>
                     launch('https://cloud.google.com/text-to-speech/docs/ssml'),
--- lib/widgets/text_to_speech/tts.dart
+++ lib/widgets/text_to_speech/tts.dart
@@ -6,6 +6,8 @@ import 'tts_api.dart';
 import 'package:intl/intl.dart';
 
 final now = DateTime.now();
+const exampleInput =
+    "Here are <say-as interpret-as=\"characters\">SSML</say-as> samples.\nI can pause <break time=\"3s\"/>.\nI can play a sound\n<audio src=\"https://rpg.hamsterrepublic.com/wiki-images/d/db/Crush8-Bit.ogg\">didn't get your MP3 audio file</audio>.\nI can speak in cardinals. Your number is <say-as interpret-as=\"cardinal\">10</say-as>.\nOr I can speak in ordinals. You are <say-as interpret-as=\"ordinal\">10</say-as> in line.\nOr I can even speak in digits. The digits for ten are <say-as interpret-as=\"characters\">10</say-as>.\nI can also substitute phrases, like the <sub alias=\"World Wide Web Consortium\">W3C</sub>.\nFinally, I can speak a paragraph with two sentences.\n<p><s>This is sentence one.</s><s>This is sentence two.</s></p>";
 
 api(text, voiceName, languageCode, fileName) {
   TextToSpeechService service = TextToSpeechService();
@@ -42,8 +44,8 @@ class TextToSpeech extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+    return const Padding(
+      padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
       child: Center(
         child: TextInput(),
       ),
@@ -62,102 +64,156 @@ class TextInput extends StatefulWidget {
 
 class _TextInputState extends State<TextInput> {
   final _formKey = GlobalKey<FormState>();
-  String _text = '', _fileName = '';
-  final inputFormTextStyle = TextStyle(color: brandBlack, fontFamily: 'Roboto');
+  String text = '', _fileName = '';
+  TextEditingController txt = TextEditingController();
+  final inputFormTextStyle =
+      const TextStyle(color: brandBlack, fontFamily: 'Roboto');
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              minLines: 1,
-              maxLines: 15,
-              style: inputFormTextStyle,
-              validator: (value) {
-                if (value!.isNotEmpty) {
-                  return null;
-                } else {
-                  return value;
-                }
-              },
-              onSaved: (value) {
-                _text = value!;
-              },
-              decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.fromLTRB(0, 16, 0, 12),
-                  focusColor: brandBlack,
-                  counterStyle: TextStyle(
-                    color: brandBlack,
-                  ),
-                  labelText: 'Enter Desired Text',
-                  labelStyle:
-                      TextStyle(color: brandBlack, fontFamily: 'Lovelo'),
-                  fillColor: brandBlack,
-                  hoverColor: brandBlack,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: brandBlack),
-                  ),
-                  constraints: BoxConstraints(maxWidth: 350, maxHeight: 100)),
-            ),
-            TextFormField(
-              style: inputFormTextStyle,
-              validator: (value) {
-                if (value!.isNotEmpty) {
-                  return null;
-                } else {
-                  return value;
-                }
-              },
-              onSaved: (value) {
-                _fileName = value!;
-              },
-              decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.fromLTRB(0, 16, 0, 12),
-                  focusColor: brandBlack,
-                  counterStyle: TextStyle(
-                    color: brandBlack,
-                  ),
-                  labelText: 'Enter File Name',
-                  labelStyle:
-                      TextStyle(color: brandBlack, fontFamily: 'Lovelo'),
-                  fillColor: brandBlack,
-                  hoverColor: brandBlack,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: brandBlack),
-                  ),
-                  constraints: BoxConstraints(maxWidth: 350, maxHeight: 100)),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: ElevatedButton(
-                //submit button
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(brandBlack)),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    log('valid inputs - saving and resetting state');
-                    _text = _text.replaceAll('"', '\\"');
-                    api(
-                        _text,
-                        'en-US-Wavenet-D', //voice
-                        'en-US',
-                        _fileName //language
-                        );
-                    _formKey.currentState!.reset();
-                  }
-                },
-                child: const Text(
-                  'Process to MP3',
-                  style: TextStyle(color: brandWhite),
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            minLines: 1,
+            maxLines: 25,
+            controller: txt,
+            style: inputFormTextStyle,
+            validator: (value) {
+              if (value!.isNotEmpty) {
+                return null;
+              } else {
+                return value;
+              }
+            },
+            onSaved: (value) {
+              text = value!;
+            },
+            decoration: const InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(0, 16, 0, 12),
+                focusColor: brandBlack,
+                counterStyle: TextStyle(
+                  color: brandBlack,
                 ),
-              ),
+                labelText: 'Enter Desired Text',
+                labelStyle: TextStyle(color: brandBlack, fontFamily: 'Lovelo'),
+                fillColor: brandBlack,
+                hoverColor: brandBlack,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: brandBlack),
+                ),
+                constraints: BoxConstraints(maxWidth: 350, maxHeight: 100)),
+          ),
+          TextFormField(
+            style: inputFormTextStyle,
+            validator: (value) {
+              if (value!.isNotEmpty) {
+                return null;
+              } else {
+                return value;
+              }
+            },
+            onSaved: (value) {
+              _fileName = value!;
+            },
+            decoration: const InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(0, 16, 0, 12),
+                focusColor: brandBlack,
+                counterStyle: TextStyle(
+                  color: brandBlack,
+                ),
+                labelText: 'Enter File Name',
+                labelStyle: TextStyle(color: brandBlack, fontFamily: 'Lovelo'),
+                fillColor: brandBlack,
+                hoverColor: brandBlack,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: brandBlack),
+                ),
+                constraints: BoxConstraints(maxWidth: 350, maxHeight: 100)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    //submit button
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(brandBlack)),
+                    onPressed: () {
+                      txt.text = txt.text + "<break time=\"1s\"/>";
+                    },
+                    child: const Text(
+                      'Add one second pause',
+                      style: TextStyle(color: brandWhite),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    //submit button
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(brandBlack)),
+                    onPressed: () {
+                      txt.text = txt.text + "<break time=\"2s\"/>";
+                    },
+                    child: const Text(
+                      'Add two second pause',
+                      style: TextStyle(color: brandWhite),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    //submit button
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.grey)),
+                    onPressed: () {
+                      txt.text = exampleInput;
+                    },
+                    child: const Text(
+                      'Try Example',
+                      style: TextStyle(color: brandWhite),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    //submit button
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(brandBlue)),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        log('valid inputs - saving and resetting state');
+                        text = text.replaceAll('"', '\\"');
+                        api(
+                            text,
+                            'en-US-Wavenet-D', //voice
+                            'en-US',
+                            _fileName //language
+                            );
+                        _formKey.currentState!.reset();
+                      }
+                    },
+                    child: const Text(
+                      'Process and Download MP3',
+                      style: TextStyle(color: brandWhite),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -166,14 +222,5 @@ class _TextInputState extends State<TextInput> {
 
 /*
 test input
-Here are <say-as interpret-as="characters">SSML</say-as> samples.
-I can pause <break time="3s"/>.
-I can play a sound
-<audio src="https://rpg.hamsterrepublic.com/wiki-images/d/db/Crush8-Bit.ogg">didn't get your MP3 audio file</audio>.
-I can speak in cardinals. Your number is <say-as interpret-as="cardinal">10</say-as>.
-Or I can speak in ordinals. You are <say-as interpret-as="ordinal">10</say-as> in line.
-Or I can even speak in digits. The digits for ten are <say-as interpret-as="characters">10</say-as>.
-I can also substitute phrases, like the <sub alias="World Wide Web Consortium">W3C</sub>.
-Finally, I can speak a paragraph with two sentences.
-<p><s>This is sentence one.</s><s>This is sentence two.</s></p>
+\nHere are <say-as interpret-as=\"characters\">SSML</say-as> samples.\nI can pause <break time=\"3s\"/>.\nI can play a sound\n<audio src=\"https://rpg.hamsterrepublic.com/wiki-images/d/db/Crush8-Bit.ogg\">didn't get your MP3 audio file</audio>.\nI can speak in cardinals. Your number is <say-as interpret-as=\"cardinal\">10</say-as>.\nOr I can speak in ordinals. You are <say-as interpret-as=\"ordinal\">10</say-as> in line.\nOr I can even speak in digits. The digits for ten are <say-as interpret-as=\"characters\">10</say-as>.\nI can also substitute phrases, like the <sub alias="World Wide Web Consortium\">W3C</sub>.\nFinally, I can speak a paragraph with two sentences.\n<p><s>This is sentence one.</s><s>This is sentence two.</s></p>
 */
