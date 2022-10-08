4aa6089f9c2725dd4e2ceccc94e7316378039f91
alex90271 <alex.alder1@gmail.com>
2022-10-07 23:40:22 -0400

    utlize single child scroll view and richtext

--- lib/views/text_to_speech/tts_view.dart
+++ lib/views/text_to_speech/tts_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../widgets/text_to_speech/tts.dart';
@@ -9,40 +10,41 @@ class TextToSpeechView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Container(
-          color: const Color.fromARGB(255, 163, 225, 253),
+        body: SingleChildScrollView(
           child: Column(
             children: [
-              const TextToSpeech(),
               const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('To add a verbal pause, click one of the above',
-                    style: TextStyle(fontFamily: 'roboto')),
+                padding: EdgeInsets.fromLTRB(0, 35.0, 0, 0),
+                child: Text('Instructions:'),
               ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "Replace the 1 or 2 within the <break time=\"2s\"/> with how many seconds to customize pause",
-                  style: TextStyle(fontFamily: 'roboto'),
-                ),
+              RichText(
+                text: TextSpan(children: [
+                  const TextSpan(text: '''
+                  \n1. Enter desired text to speech in text box below
+                  \n2. Enter file name to download as, the date and time will be added to filename
+                  \n3. To add any pauses for more natural sounding speach, use the black 'Add Pause' buttons
+                  ''', style: TextStyle(fontWeight: FontWeight.bold)),
+                  const TextSpan(
+                    text: '''
+                  \nPauses can be customized, just change the number inside the < > to desired length (200ms recommended)
+                  \nClick 'Try Example' to insert an example snipet for a demonstration
+                  \nFor more advance customization, use SSML encoding. For more information visit:
+                  ''',
+                    style: TextStyle(fontFamily: 'roboto'),
+                  ),
+                  TextSpan(
+                      text: 'https://cloud.google.com/text-to-speech/docs/ssml',
+                      style: const TextStyle(
+                          fontFamily: 'roboto',
+                          decoration: TextDecoration.underline),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = (() {
+                          launch(
+                              'https://cloud.google.com/text-to-speech/docs/ssml');
+                        })),
+                ]),
               ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "For further customization, use SSML. For more information on SSML visit:",
-                  style: TextStyle(fontFamily: 'roboto'),
-                ),
-              ),
-              InkWell(
-                child: const Text(
-                  'https://cloud.google.com/text-to-speech/docs/ssml',
-                  style: TextStyle(
-                      fontFamily: 'roboto',
-                      decoration: TextDecoration.underline),
-                ),
-                onTap: () =>
-                    launch('https://cloud.google.com/text-to-speech/docs/ssml'),
-              )
+              const TextToSpeech()
             ],
           ),
         ));
