a376662a6e27e4ff6c5ff519e2ee4de6301c38de
alex90271 <alex.alder1@gmail.com>
2022-10-09 14:05:00 -0400

    fix padding and colors

--- lib/views/text_to_speech/tts_view.dart
+++ lib/views/text_to_speech/tts_view.dart
@@ -9,7 +9,7 @@ class TextToSpeechView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomInset: true,
         body: SingleChildScrollView(
           child: Column(
             children: [
@@ -17,32 +17,43 @@ class TextToSpeechView extends StatelessWidget {
                 padding: EdgeInsets.fromLTRB(0, 35.0, 0, 0),
                 child: Text('Instructions:'),
               ),
-              RichText(
-                text: TextSpan(children: [
-                  const TextSpan(text: '''
-                  \n1. Enter desired text to speech in text box below
-                  \n2. Enter file name to download as, the date and time will be added to filename
-                  \n3. To add any pauses for more natural sounding speach, use the black 'Add Pause' buttons
-                  ''', style: TextStyle(fontWeight: FontWeight.bold)),
-                  const TextSpan(
-                    text: '''
-                  \nPauses can be customized, just change the number inside the < > to desired length (200ms recommended)
-                  \nClick 'Try Example' to insert an example snipet for a demonstration
-                  \nFor more advance customization, use SSML encoding. For more information visit:
-                  ''',
-                    style: TextStyle(fontFamily: 'roboto'),
-                  ),
-                  TextSpan(
-                      text: 'https://cloud.google.com/text-to-speech/docs/ssml',
-                      style: const TextStyle(
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                child: RichText(
+                  text: TextSpan(children: [
+                    const TextSpan(
+                        text: '''
+                    \n1. Enter desired text to speech in text box below
+                    \n2. Enter file name to download as, the date and time will be added to filename
+                    \n3. To add any pauses for more natural sounding speach, use the black 'Add Pause' buttons
+                    ''',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 0, 0, 0))),
+                    const TextSpan(
+                      text: '''
+                    \nPauses can be customized, just change the number inside the < > to desired length (200ms recommended)
+                    \nClick 'Try Example' to insert an example snipet for a demonstration
+                    \nFor more advance customization, use SSML encoding. For more information visit:
+                    ''',
+                      style: TextStyle(
                           fontFamily: 'roboto',
-                          decoration: TextDecoration.underline),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = (() {
-                          launch(
-                              'https://cloud.google.com/text-to-speech/docs/ssml');
-                        })),
-                ]),
+                          color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                    TextSpan(
+                        text:
+                            '\nhttps://cloud.google.com/text-to-speech/docs/ssml',
+                        style: const TextStyle(
+                            fontFamily: 'roboto',
+                            decoration: TextDecoration.underline,
+                            color: Color.fromARGB(255, 0, 0, 0)),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = (() {
+                            launch(
+                                'https://cloud.google.com/text-to-speech/docs/ssml');
+                          })),
+                  ]),
+                ),
               ),
               const TextToSpeech()
             ],
